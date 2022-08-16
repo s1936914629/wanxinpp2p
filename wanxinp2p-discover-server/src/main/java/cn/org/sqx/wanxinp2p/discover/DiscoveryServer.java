5c93fd22ce1c7c2d422b8d7a2de86f10290745c9
s1936914629 <1936914629@qq.com>
2022-08-16 10:17:57 +0800

    项目启动修复

--- wanxinp2p-discover-server/src/main/java/cn/org/sqx/wanxinp2p/discover/DiscoveryServer.java
+++ wanxinp2p-discover-server/src/main/java/cn/org/sqx/wanxinp2p/discover/DiscoveryServer.java
@@ -1,15 +1,17 @@
+
 package cn.org.sqx.wanxinp2p.discover;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServer.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServer.class, args);
 
-    }
+	}
 }
